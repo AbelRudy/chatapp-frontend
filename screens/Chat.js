b2110d27e5766a316bab868165f817e9e3296fe9
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-02-12 18:19:09 +0100

    Add textbox input and send button

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,23 +1,90 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import BackgroundImage from "../assets/background.png"
+import { Ionicons } from "@expo/vector-icons";
+import React, { useCallback, useState } from "react";
+import {
+	ImageBackground,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import BackgroundImage from "../assets/background.png";
+import Colors from "../utils/Colors";
 
 export default function Chat() {
+	const [message, setMessage] = useState("");
+
+	const onChangeText = (text) => {
+		setMessage(text);
+	};
+
+	const onSubmit = useCallback(() => {
+		console.log("Send");
+		setMessage("");
+	}, [message]);
+
 	return (
-		<ImageBackground source={BackgroundImage} style={styles.BackgroundImage}>
-			<View style={styles.container}>
-				<Text>Chat</Text>
-			</View>
-		</ImageBackground>
+		<SafeAreaView style={styles.container} edges={["bottom"]}>
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				behavior={Platform.select({ android: undefined, ios: "padding" })}
+                keyboardVerticalOffset={100}
+			>
+				<ImageBackground
+					source={BackgroundImage}
+					style={styles.BackgroundImage}
+				/>
+				<View style={styles.inputContainer}>
+					<TextInput
+						value={message}
+						style={styles.textBox}
+						onChangeText={onChangeText}
+						onSubmitEditing={onSubmit}
+					/>
+					<TouchableOpacity
+						onPress={onSubmit}
+						style={styles.sendButton}
+						disabled={message.length === 0}
+					>
+						<Ionicons name="send" size={20} color="white" />
+					</TouchableOpacity>
+				</View>
+			</KeyboardAvoidingView>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-        flexDirection: 'column'
+		flexDirection: "column",
+		backgroundColor: "#fff",
 	},
-    BackgroundImage: {
-        flex: 1
-    }
+	BackgroundImage: {
+		flex: 1,
+	},
+	inputContainer: {
+		flexDirection: "row",
+		paddingVertical: 8,
+		paddingHorizontal: 10,
+		height: 50,
+	},
+	textBox: {
+		flex: 1,
+		borderWidth: 1,
+		borderRadius: 50,
+		borderColor: Colors.lightGrey,
+		marginRight: 15,
+		paddingHorizontal: 12,
+	},
+	sendButton: {
+		backgroundColor: Colors.primary,
+		justifyContent: "center",
+		alignItems: "center",
+		width: 50,
+		padding: 5,
+		borderRadius: 50,
+	},
 });
---
+++ utils/Colors.js
@@ -0,0 +1,7 @@
+const Colors = {
+	primary: "#0388A7",
+	secondary: "#B3DCE5",
+	lightGrey: "#bdc3c7",
+};
+
+export default Colors;
