29d4d002ecceb80b7596212ed0094f0995c338b0
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-03-02 09:52:10 +0100

    Handle changes in signup form

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,10 +1,23 @@
 import { Entypo, FontAwesome, FontAwesome5 } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import Logo from "../assets/logo.png";
 import Input from "./Input";
 
 export default function SignUpForm() {
+	const [formData, setFormData] = useState({
+		username: "",
+		email: "",
+		password: "",
+	});
+
+	const onChangeText = (id, text) => {
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[id]: text,
+		}));
+	};
+
 	return (
 		<>
 			<View style={styles.headerContainer}>
@@ -17,22 +30,29 @@ export default function SignUpForm() {
 				<Text style={styles.headerText}>Bienvenue parmi nous.</Text>
 			</View>
 			<Input
+				id="username"
 				label="Nom d'utilisateur"
 				keyboardType="default"
 				withIcon={true}
 				iconName="user"
 				iconSize={20}
 				IconPack={FontAwesome}
+				value={formData.username}
+				onChangeText={onChangeText}
 			/>
 			<Input
+				id="email"
 				label="Adresse e-mail"
 				keyboardType="email-address"
 				withIcon={true}
 				iconName="mail"
 				iconSize={20}
 				IconPack={Entypo}
+				value={formData.email}
+				onChangeText={onChangeText}
 			/>
 			<Input
+				id="password"
 				label="Mot de passe"
 				keyboardType="default"
 				secureTextEntry={true}
@@ -40,6 +60,8 @@ export default function SignUpForm() {
 				iconName="key"
 				iconSize={20}
 				IconPack={FontAwesome5}
+				value={formData.password}
+				onChangeText={onChangeText}
 			/>
 		</>
 	);
