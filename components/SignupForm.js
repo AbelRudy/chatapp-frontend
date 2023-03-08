330821ae3f13a346805207de704a3ae78f35bfa8
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-03-08 10:39:22 +0100

    Validate and submit form

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,133 +1,151 @@
 import { Entypo, FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import React, { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-import validator from "validator"
+import validator from "validator";
 import Logo from "../assets/logo.png";
+import http from "../utils/httpClient";
+import ShowToast from "../utils/ShowToast";
 import Input from "./Input";
 
 export default function SignUpForm() {
-	const [formData, setFormData] = useState({
-		username: "",
-		email: "",
-		password: "",
-	});
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
 
-	const [errorText, setErrorText] = useState({
-		username:"",
-		email:"",
-		password:""
-	})
+    const [errorText, setErrorText] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
 
-	const onChangeText = (id, text) => {
-		setFormData((prevFormData) => ({
-			...prevFormData,
-			[id]: id !== "password" ? text.trim() : text,
-		}));
-	};
+    const onChangeText = (id, text) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [id]: id !== "password" ? text.trim() : text,
+        }));
+    };
 
-	const onEndEditing = (id, text) => {
-		setErrorText((prevErrorText) => ({
-			...prevErrorText,
-			[id]: validateField(id, text),
-		}))
-	}
+    const onEndEditing = (id, text) => {
+        setErrorText((prevErrorText) => ({
+            ...prevErrorText,
+            [id]: validateField(id, text),
+        }));
+    };
 
-	const validateField = (id, text) => {
-		switch (id) {
-			case "username":
-				if(validator.isEmpty(text))
-					return "Le nom d'utilisateur ne peut pas être vide.";
-				if(!validator.matches(text, /^[a-zA-Z0-9]{3,50}$/))
-					return "Le nom d'utilisateu ne doit contenir que des lettres et des chiffres entre 3 et 50 caractères.";
-				return null;
-			case "email":
-				if(validator.isEmpty(text))
-					return "L'adresse email ne peut pas être vide.";
-				if(!validator.isEmail(text))
-					return "L'adresse email saisie n'est pas valide.";
-				return null;
-			case "password":
-				if(validator.isEmpty(text))
-					return "Le mot de passe ne peut pas être vide.";
-				if(!validator.matches(text, /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/))
-					return "Entre 8 et 15 caractères\nAu moins 1 majuscule\nau moins 1 minuscule\nAu moins un chiffre\nAu moins un caractère spécial"
-				return null;
-			default:
-				break;
-		}
-	}
+    const validateField = (id, text) => {
+        switch (id) {
+            case "username":
+                if (validator.isEmpty(text))
+                    return "Le nom d'utilisateur ne peut pas être vide.";
+                if (!validator.matches(text, /^[a-zA-Z0-9]{3,50}$/))
+                    return "Le nom d'utilisateu ne doit contenir que des lettres et des chiffres entre 3 et 50 caractères.";
+                return null;
+            case "email":
+                if (validator.isEmpty(text))
+                    return "L'adresse email ne peut pas être vide.";
+                if (!validator.isEmail(text))
+                    return "L'adresse email saisie n'est pas valide.";
+                return null;
+            case "password":
+                if (validator.isEmpty(text))
+                    return "Le mot de passe ne peut pas être vide.";
+                if (
+                    !validator.matches(
+                        text,
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/
+                    )
+                )
+                    return "Entre 8 et 15 caractères\nAu moins 1 majuscule\nau moins 1 minuscule\nAu moins un chiffre\nAu moins un caractère spécial";
+                return null;
+            default:
+                break;
+        }
+    };
 
-	const onSubmit = () => {
-		// if(validator.isEmpty())
-	}
+    const onSubmitEditing = async () => {
+        let isFormValid = true;
+        Object.keys(formData).forEach((key) => {
+            if (validateField(key, formData[key])) isFormValid = false;
+        });
+        if (!isFormValid) {
+            ShowToast("Le formulaire n'est pas valide.");
+        } else {
+            await http.post("user/signup", { ...formData });
+        }
+    };
 
-	return (
-		<>
-			<View style={styles.headerContainer}>
-				<Image
-					source={Logo}
-					resizeMode="contain"
-					resizeMethod="resize"
-					style={styles.headerImage}
-				/>
-				<Text style={styles.headerText}>Bienvenue parmi nous.</Text>
-			</View>
-			<Input
-				id="username"
-				label="Nom d'utilisateur"
-				keyboardType="default"
-				withIcon={true}
-				iconName="user"
-				iconSize={20}
-				IconPack={FontAwesome}
-				value={formData.username}
-				onChangeText={onChangeText}
-				errorText={errorText.username}
-				onEndEditing={onEndEditing}
-			/>
-			<Input
-				id="email"
-				label="Adresse e-mail"
-				keyboardType="email-address"
-				withIcon={true}
-				iconName="mail"
-				iconSize={20}
-				IconPack={Entypo}
-				value={formData.email}
-				onChangeText={onChangeText}
-				errorText={errorText.email}
-				onEndEditing={onEndEditing}
-			/>
-			<Input
-				id="password"
-				label="Mot de passe"
-				keyboardType="default"
-				secureTextEntry={true}
-				withIcon={true}
-				iconName="key"
-				iconSize={20}
-				IconPack={FontAwesome5}
-				value={formData.password}
-				onChangeText={onChangeText}
-				errorText={errorText.password}
-				onEndEditing={onEndEditing}
-			/>
-		</>
-	);
+    return (
+        <>
+            <View style={styles.headerContainer}>
+                <Image
+                    source={Logo}
+                    resizeMode="contain"
+                    resizeMethod="resize"
+                    style={styles.headerImage}
+                />
+                <Text style={styles.headerText}>Bienvenue parmi nous.</Text>
+            </View>
+            <Input
+                id="username"
+                label="Nom d'utilisateur"
+                keyboardType="default"
+                withIcon={true}
+                iconName="user"
+                iconSize={20}
+                IconPack={FontAwesome}
+                value={formData.username}
+                onChangeText={onChangeText}
+                errorText={errorText.username}
+                onEndEditing={onEndEditing}
+                onSubmitEditing={onSubmitEditing}
+            />
+            <Input
+                id="email"
+                label="Adresse e-mail"
+                keyboardType="email-address"
+                withIcon={true}
+                iconName="mail"
+                iconSize={20}
+                IconPack={Entypo}
+                value={formData.email}
+                onChangeText={onChangeText}
+                errorText={errorText.email}
+                onEndEditing={onEndEditing}
+                onSubmitEditing={onSubmitEditing}
+            />
+            <Input
+                id="password"
+                label="Mot de passe"
+                keyboardType="default"
+                secureTextEntry={true}
+                withIcon={true}
+                iconName="key"
+                iconSize={20}
+                IconPack={FontAwesome5}
+                value={formData.password}
+                onChangeText={onChangeText}
+                errorText={errorText.password}
+                onEndEditing={onEndEditing}
+                onSubmitEditing={onSubmitEditing}
+            />
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-	headerContainer: {
-		alignItems: "center",
-		height: "30%",
-		marginBottom: 10,
-	},
-	headerImage: {
-		width: "50%",
-		height: "50%",
-	},
-	headerText: {
-		fontSize: 20,
-		marginTop: 30,
-	},
+    headerContainer: {
+        alignItems: "center",
+        height: "30%",
+        marginBottom: 10,
+    },
+    headerImage: {
+        width: "50%",
+        height: "50%",
+    },
+    headerText: {
+        fontSize: 20,
+        marginTop: 30,
+    },
 });
